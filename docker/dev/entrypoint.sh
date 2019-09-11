190774f106752e932b65621de9a7df7f60c24560
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-09-11 09:10:00 +0100

    create dockerfile and makefile

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -4,12 +4,14 @@ printf "\n\n======================================\n"
 printf "Making database migrations"
 printf "\n======================================\n\n"
 
-python3 run.py db migrate
+cd src
+python3 manage.py makemigrations 
+python3 manage.py migrate 
 
 printf "\n\n======================================\n"
 printf "Start the application"
 printf "\n======================================\n\n"
 
-python3 run.py runserver
+python3 manage.py runserver 0.0.0.0:8000
 
 exit 0
--- docker/tests/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-printf "\n\n======================================\n"
-printf "Running linting checks and unit tests"
-printf "\n======================================\n\n"
-
-tox -e coverage
-
-exit 0
