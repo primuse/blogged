84b6530ae5875b95d71adcf03c6a7a90e8b223cb
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-09-11 08:05:10 +0100

    add templates

--- src/blogged/views.py
+++ src/blogged/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-  return HttpResponse("<h1>Hello</h1>")
+  return render(request, 'index.html')
 
 def about_page(request):
-  return HttpResponse("<h1>About Us</h1>")
+  return render(request, 'index.html')
 
 def contact_page(request):
-  return HttpResponse("<h1>Contact Us</h1>")
+  return render(request, 'index.html')
